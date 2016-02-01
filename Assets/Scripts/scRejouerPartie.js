f06c603ca227e6e1cbc318e60ef7f04fb20016dd
crisman7 <gabrielcris@gmail.com>
2016-02-01 18:58:50 -0500

    ok pour modif

---
+++ Assets/Scripts/scRejouerPartie.js
@@ -0,0 +1,33 @@
+#pragma strict
+
+/**
+ * Dev Jeu
+ * Permet de rejouer au jeu
+ * @author stéphane Leclerc
+ * @author Jonathan Martel
+ * @date 2016-01-29
+ * 
+ */
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+//::::::::::::::::::::function pour rejouer une partie après le game over:::::::::::::://
+
+function rejouerUnePartie()
+{
+    SceneManager.LoadScene("niveau1");
+}
+
+//::::::::::::::::::::function pour retourné au menu:::::::::::::://
+
+function retournerMenu()
+{
+    SceneManager.LoadScene("menu");
+}
