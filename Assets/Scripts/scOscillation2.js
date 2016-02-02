c942968e09f3e68318a3bf2e36774ff5049d72a0
cmahneke <cmahneke@gmail.com>
2016-02-02 00:37:32 -0500

    assets niveau1 et tuto complet

---
+++ Assets/Scripts/scOscillation2.js
@@ -0,0 +1,20 @@
+#pragma strict
+/**
+*Script pour translader un objet sur un axe entre deux points, dans ce cas des vagues du ruisseau
+*@author Cristina Mahneke
+*@source http://answers.unity3d.com/questions/8781/oscillating-variable.html
+*@date 01/02/2016
+**/
+
+
+
+function Start () {
+
+
+}
+
+function Update () {
+	
+		transform.position.x = -Mathf.PingPong(Time.time*.2, .07) - .07;
+
+}
