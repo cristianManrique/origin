84dff1ba8c1ee024d3287f96564f3928ec6bcf92
cmahneke <cmahneke@gmail.com>
2016-03-06 22:14:21 -0500

    sons boutons menus

---
+++ Assets/Scripts/scToggleMusique.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+
+
+private var sourceSon: AudioSource;
+
+private var jouer: boolean;
+
+
+function Start () {
+	sourceSon = GetComponent.<AudioSource>();
+	jouer = true;
+}
+
+function OnClick(){
+
+	if(jouer==true){
+
+		
+		sourceSon.Stop();
+
+		jouer = false;
+
+	}else if(jouer == false){
+
+
+		sourceSon.Play();
+
+		jouer = true;
+
+	}
+}
