9ebdd52d29a4aa930fa01c1cf3ccc76c8abb4422
davidlachambre <davidlach@hotmail.com>
2016-03-07 18:48:36 -0500

    Modification des scènes et bg pour les menus principal/gagnant/perdant + correction de bugs mineurs

--- Assets/Scripts/scBgVideo.js
+++ Assets/Scripts/scBgVideo.js
@@ -27,17 +27,17 @@ private var video:MovieTexture;
 * @access private
 * @var String
 */
-private var nomVideo:String = "bgMenu";
+public var nomVideo:String;
 
 function Awake() {
     textureVideo = this.GetComponent(GUITexture);//Récupère le composant GUITexture.  
     //load the movie texture from the resources folder  
-    video = Resources.Load(nomVideo) as MovieTexture;//Chargement du vidéo à partir du dossier Resources.
+    video = Resources.Load("Video/" + nomVideo) as MovieTexture;//Chargement du vidéo à partir du dossier Resources.
     //anamorphic fullscreen  
     textureVideo.pixelInset = new Rect(Screen.width/2, -Screen.height/2,0,0);//Pour mettre le vidéo en anamorphic plein écran.
 }
 
 function Start () {
-//    video.loop = true;
-//    video.Play();
+    video.loop = true;
+    video.Play();
 }
